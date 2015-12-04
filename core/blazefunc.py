ac9d074aef8038558b2449e9f9ab1f7442d2e3c3
Per <per.calissendorff@live.se>
2015-12-04 11:56:36 +0100

    Updated blazefunction angles theta name convention to match excel-arc.

--- core/blazefunc.py
+++ core/blazefunc.py
@@ -24,34 +24,31 @@ def blaze_func(wavelength, m, Echelle_angle, sigma_echelle_inv, gamma_echelle, b
 	
 	""" Converting variabels to proper units """
 	sigma_ech = 1e6/sigma_echelle_inv		# nm
-	alpha_ech = Echelle_angle * np.pi/180	# rad
+	alpha = Echelle_angle * np.pi/180		# rad
 	gamma_ech = gamma_echelle * np.pi/180	# rad
-	blaze_angle = blaze_angle * np.pi/180	# rad
-	theta_blaze = alpha_ech - blaze_angle	# rad
+	theta_blaze = blaze_angle * np.pi/180	# rad
+	theta = alpha - theta_blaze				# rad
 	D_alpha_ech = -3.23 * np.pi/180			# rad
-
-
-	""" Approximated values more closely resembling Tino's excel-arc. """
-	#pi = 3.14159267
-	#sigma_ech = 31600
-	#alpha_ech=Echelle_angle * pi/180
-	#gamma_ech = 3.8 * pi/180
-	#blaze_angle = 63.76 * pi/180
-	#theta_blaze=alpha_ech-blaze_angle
-	#D_alpha_ech = -3.23 * pi/180
-
+	
+	""" Approximated values obtained from Tino's excel-arc. """
+	#pi = 3.1415927
+	#sigma_ech = 31600						# nm
+	#alpha = Echelle_angle * pi/180			# rad
+	#gamma_ech = gamma_echelle * pi/180		# rad
+	#theta_blaze = blaze_angle * pi/180		# rad
+	#theta = alpha - theta_blaze			# rad
+	#D_alpha_ech = -3.23 * pi/180			# rad	
 
 	""" Blaze function that mimics the equations from Tino's excel-arc. """
-#	beta=np.arcsin(wavelength * m / sigma_ech - np.sin(alpha_ech)) - D_alpha_ech - alpha_ech
-#	nu = np.pi * sigma_ech * np.cos(alpha_ech) * (np.sin(theta_blaze) + np.sin(alpha_ech + D_alpha_ech + beta - blaze_angle)) / (wavelength * np.cos(theta_blaze))
-
+#	beta = np.arcsin(wavelength * m / sigma_ech - np.sin(alpha)) - D_alpha_ech - alpha
+#	nu = np.pi * sigma_ech * np.cos(alpha) * (np.sin(theta) + np.sin(alpha + D_alpha_ech + beta - theta_blaze)) / (wavelength * np.cos(theta))
 
 	""" Alternative version of the blaze-function. 
 	More closely aligned on the blaze wavelength than Tino's version. """
-	beta = np.arcsin( (m * wavelength) / (sigma_ech * np.cos(gamma_ech)) - np.sin(alpha_ech))
-	nu = (np.pi * sigma_ech * np.cos(blaze_angle) / wavelength) * (np.sin(beta - blaze_angle) + np.sin(theta_blaze))
+	beta = np.arcsin( (m * wavelength) / (sigma_ech * np.cos(gamma_ech)) - np.sin(alpha))	
+	nu = (np.pi * sigma_ech / wavelength) * (np.sin(beta-theta_blaze) + np.sin(alpha-theta_blaze))
 
 	blaze_eff = (np.sin(nu) * np.sin(nu)) / (nu * nu)
-	lambda_blaze = 2 * sigma_ech * np.cos(gamma_ech) * np.sin(blaze_angle) / m	
+	lambda_blaze = 2 * sigma_ech * np.cos(gamma_ech) * np.cos(theta) * np.sin(theta_blaze) / m
 
 	return blaze_eff, lambda_blaze
