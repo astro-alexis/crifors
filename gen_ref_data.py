fe2dd9f3d36ad86cb2a0bf143287f3c4cf500fa6
Thomas Marquart <thomas@marquart.se>
2016-11-04 12:57:48 +0100

    loop over settings instead of bands, also take settings as command line arguments now.

--- gen_ref_data.py
+++ gen_ref_data.py
@@ -12,19 +12,25 @@ NRAYS = '1E6'
 OPTS = ['--spread', '--nrays=%s'%NRAYS, '--blaze']
 
 # LOOPS
-bands = ['Y', 'J', 'H', 'K', 'L', 'M']
+# bands = ['Y', 'J', 'H', 'K', 'L', 'M']
 psfs = ['gaussian', 'polarimeter', 'uniform', 'decker1', 'decker2']
 inspectra = ['P', 'F', 'E']#, 'T']
+
+if len(sys.argv) < 2:
+    std_settings = crifors.paths.std_settings.keys()
+else:
+    std_settings = sys.argv[1:]
 
 # LOGGING
 #logger = logging.getLogger()
 #formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 #logger.setLevel(logging.DEBUG)
 
-for band in bands:
+for sett in std_settings:
  for spec in inspectra:
   for psf in psfs:
-    fname = FNAME_BASE + '%s_%s_%s'%(band,spec,psf) # '.fits.gz' gets added by crifors
+    sname = sett.replace('/','')
+    fname = FNAME_BASE + '%s_%s_%s'%(sname,spec,psf) # '.fits.gz' gets added by crifors
     print fname
     for ext in ['.log', '.fits.gz']:
         if os.path.exists(fname+ext):
@@ -33,7 +39,7 @@ for band in bands:
 #    hdlr.setFormatter(formatter)
 #    logger.addHandler(hdlr)
 
-    argv = [band, spec, "--psf=%s"%psf, "--outfn='%s'"%fname] + OPTS
+    argv = [sett, spec, "--psf=%s"%psf, "--outfn='%s'"%fname] + OPTS
     subprocess.call(['./crifors.py'] + argv)
 
 
