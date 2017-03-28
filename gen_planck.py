215662c228f1fd5bd21fa4e940c11b8fd894808a
Thomas Marquart <thomas@marquart.se>
2017-03-28 10:50:42 +0200

    higher res flat

--- gen_planck.py
+++ gen_planck.py
@@ -3,7 +3,7 @@
 import sys, os
 from pylab import *
 
-wav = linspace(850E-9, 5000E-9, 1E5)
+wav = linspace(1400E-9, 2000E-9, 2E5)
 hc2=1.191043E-16 #kg m^4/s^3
 hc=1.98E-25 # Jm
 K=1.38E-23 # J/K
@@ -14,7 +14,8 @@ def planck(wav,temp):
 pl = planck(wav,800)+ planck(wav,3000)/800
 pl /= pl.max()
 
-fname = 'data/spectra/flatfield/twoplanck.dat'
+args = sys.argv[1:]
+fname = args[0] if args else 'data/spectra/flatfield/twoplanck.dat'
 savetxt(fname, zip(wav*1E9,pl), delimiter=' ', newline='\n', fmt='%.5e')
 
 
