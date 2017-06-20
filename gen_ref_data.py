aec391894a88b68e85be4b9e9672acd0fa1810c1
Thomas Marquart <thomas@marquart.se>
2017-06-20 09:41:51 +0200

    tweak scripts with multiprocessing

--- gen_ref_data.py
+++ gen_ref_data.py
@@ -2,14 +2,15 @@
 
 import sys, os
 import logging
-import subprocess
 import crifors
+import multiprocessing
+import subprocess
 
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 OUTDIR = os.path.join(SCRIPTDIR, 'refdata')
 FNAME_BASE = os.path.join(OUTDIR,'CRIFORS_')
-NRAYS = '1E7'
-OPTS = ['--spread', '--nrays=%s'%NRAYS, '--blaze']
+NRAYS = '1E9'
+OPTS = ['--spread', '--nrays=%s'%NRAYS, '--blaze', '--noise']
 
 setups = [\
 #['P','gaussian'],
@@ -18,9 +19,9 @@ setups = [\
 ['F','uniform'],
 #['F','decker1'],
 #['F','decker2'],
-['E','uniform'],
+#['E','uniform'],
 #['E','decker2'],
-['T','uniform'],
+#['T','uniform'],
 #['T','decker2'],
 
 ]
@@ -30,14 +31,19 @@ if len(sys.argv) < 2:
 else:
     std_settings = sys.argv[1:]
 
+cmd_list = []
+
 for sett in std_settings:
     #if sett.startswith('L') or sett.startswith('M'):
     #    continue     # skip L & M for now
 
     for setup in setups:
         spec, psf = setup
+
         if sett.startswith('Y') and psf.upper() == 'T':
             continue # The ThAr starts in J band only.
+        #if sett.startswith('L') or sett.startswith('M'): continue
+
         sname = sett.replace('/','')
         fname = FNAME_BASE + '%s_%s_%s'%(sname,spec,psf) # '.fits.gz' gets added by crifors
         if os.path.exists(fname+'.fits.gz'):
@@ -45,8 +51,22 @@ for sett in std_settings:
             print 'skip, %s exists'%fname
             continue
 
-        argv = ["--psf=%s"%psf, "--outfn='%s'"%fname] + OPTS + [sett, spec]
-        subprocess.call(['./crifors.py'] + argv)
+        argv = ["--psf=%s"%psf, '--outfn="%s"'%fname] + OPTS + [sett, spec]
+        cmd_list.append(argv)
+
+
+log = crifors.prepare_logging()
+def work(cmd):
+    crifors.main(log,cmd)
+    #return subprocess.call(cmd, shell=True)
+
+
+print cmd_list
+
+
+pool = multiprocessing.Pool(4)
+
+pool.map(work, cmd_list)
 
 
 #    logger.removeHandler(hdlr)
--- run_traces.py
+++ run_traces.py
@@ -7,8 +7,12 @@ import subprocess
 def work(cmd):
     return subprocess.call(cmd, shell=True)
 
-pool = multiprocessing.Pool(8)
+pool = multiprocessing.Pool(12)
 
 flats = glob.glob('CRIFORS_*_F_uniform.fits')
+for flat in flats:
+    if os.path.exists(flat.replace('.fits','_trace.fits')):
+        flats.remove(flat)
+
 pool.map(work, ['esorex cr2res_util_trace '+flat for flat in flats])
 
