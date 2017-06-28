1ec441f2d1d8647a98b57f0e25c36c8b58412020
Thomas Marquart <thomas@marquart.se>
2017-06-28 09:34:53 +0200

    no gzip by default

--- core/write2fits.py
+++ core/write2fits.py
@@ -202,7 +202,7 @@ def add_default_keywords(header):
     header['HIERARCH ESO DPR TECH'] = ('SPECTRUM')
 
 
-def write_to_fits(sim, gzip=True):
+def write_to_fits(sim, gzip=False):
     """Write simulation results to fits file.
 
     Write header and data to FITS HDU. Compress if necessary.
