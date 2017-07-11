4b55842a5916ae8068e4e66e081153905819fd58
Thomas Marquart <thomas@marquart.se>
2017-07-11 11:36:17 +0200

    let run_traces.py also do the extracions and wavecals

--- run_traces.py
+++ run_traces.py
@@ -9,10 +9,40 @@ def work(cmd):
 
 pool = multiprocessing.Pool(12)
 
-flats = glob.glob('CRIFORS_*_F_uniform.fits')
-for flat in flats:
-    if os.path.exists(flat.replace('.fits','_trace.fits')):
-        flats.remove(flat)
+files = glob.glob('CRIFORS_*_F_uniform.fits')
+files += glob.glob('CRIFORS_*_F_decker1.fits')
+files += glob.glob('CRIFORS_*_F_decker2.fits')
+for file in files:
+    if os.path.exists(file.replace('.fits','_trace.fits')):
+        files.remove(file)
 
-pool.map(work, ['esorex cr2res_util_trace '+flat for flat in flats])
+pool.map(work, ['esorex cr2res_util_trace '+file for file in files])
+
+files = glob.glob('CRIFORS_*_trace.fits')
+for file in files:
+    if os.path.exists(file.replace('_trace.fits','_trace_extract1D.fits')):
+        files.remove(file)
+
+pool.map(work, ['esorex cr2res_util_extract --height=20 %s %s'%(file.replace('_F_','_E_').replace('_trace',''), 
+                        file) for file in files])
+pool.map(work, ['esorex cr2res_util_extract --height=20 %s %s'%(file.replace('_F_','_G_').replace('_trace',''), 
+                        file) for file in files])
+pool.map(work, ['esorex cr2res_util_extract --height=20 %s %s'%(file.replace('_F_','_T_').replace('_trace',''), 
+                        file) for file in files])
+
+
+files = glob.glob('CRIFORS_*_extract1D.fits')
+for file in files:
+    if os.path.exists(file.replace('_extract1D.fits','_extract1D_wave.fits')):
+        files.remove(file)
+
+gas = [file in files if '_G_' in file]
+etas = [file in files if '_E_' in file]
+lines = [file in files if '_T_' in file]
+pool.map(work, ['esorex cr2res_util_wave --data_type="LAMP" %s %s %s'\
+        %(file, file.replace('_T_','_F_').replace('_extract1D','_trace'), 'cr2res_util_genlines.fits')
+                        for file in lines])
+pool.map(work, ['esorex cr2res_util_wave --data_type="ETALON" %s %s'\
+        %(file, file.replace('_E_','_F_').replace('_extract1D','_trace') )
+                        for file in lines])
 
